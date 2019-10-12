d9306c4d31943b7b4851ec28853a9f68f24ab4ed
zhougao <2885253480@qq.coom>
2019-10-12 17:47:06 +0800

    添加一些注释

--- myM3u8/src/main/java/com/m3u8/zg/utils/QQLiveHtmlUtils.java
+++ myM3u8/src/main/java/com/m3u8/zg/utils/QQLiveHtmlUtils.java
@@ -12,18 +12,41 @@ import org.jsoup.nodes.Document;
  */
 public class QQLiveHtmlUtils {
 
+	/**
+	 * 获取视频页面html文件
+	 * @Title openHtml
+	 * @Description  
+	 * @param documentUrl
+	 * @return 
+	 * @since 2019年10月12日 下午5:46:08
+	 */
 	public static Document openHtml(String documentUrl){
 		Document root = DocumentToolkit.getDocument(documentUrl, 1);
 		System.out.println(root);
 		return root;
 	}
 	
+	/**
+	 * 获取视频页面js变量
+	 * @Title getJs
+	 * @Description  
+	 * @param documentUrl
+	 * @return 
+	 * @since 2019年10月12日 下午5:46:22
+	 */
 	public static String getJs(String documentUrl){
 		String rootJs = DocumentToolkit.getJSVariable(documentUrl, "COVER_INFO");
 		System.out.println(rootJs);
 		return rootJs;
 	}
 	
+	/**
+	 * 获取m3u8的请求参数和地址
+	 * @Title getM3u8Url
+	 * @Description  
+	 * @return 
+	 * @since 2019年10月12日 下午5:46:33
+	 */
 	public static String getM3u8Url(){
 		//腾讯的代理地址
 		String proxyhttp = "https://vi.l.qq.com/proxyhttp";
